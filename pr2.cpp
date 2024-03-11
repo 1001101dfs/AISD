c6e3b375a018d83d6bae9a2239d13acdebe5c895
1001101dfs <125999918+1001101dfs@users.noreply.github.com>
2024-03-11 16:31:06 +0300

    Add files via upload

---
+++ pr2.cpp
@@ -0,0 +1,40 @@
+﻿#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("Ofast")
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <set>
+#include <algorithm>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <climits>
+#include <ctime>
+#include <queue>
+#include <map>
+#include <stack>
+#include <unordered_set>
+#define ll long long
+#define f .first
+#define s .second
+#define mp make_pair
+#define pb push_back
+using namespace std;
+
+int main() {
+	ifstream fin("input.txt");
+	ofstream fout("output.txt");
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ll n, x, y, minx = LLONG_MAX, miny = LLONG_MAX , maxx= LLONG_MIN, maxy = LLONG_MIN;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> x >> y;
+		minx = min(minx, x);
+		miny = min(miny, y);
+		maxx = max(maxx, x);
+		maxy = max(maxy, y);
+	}
+	cout << minx << ' ' << miny << ' ' << maxx << ' ' << maxy;
+}
